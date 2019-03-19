80bc3539a3b356527463c7803c4c2fc1cb2e75f8
Vitor Boccio <vitorboccio@gmail.com>
2019-03-19 21:06:02 +1100

    handling error

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,13 @@
 import React from "react";
+import GoogleLogin from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = (googleUser) => {
+    console.log('on success', googleUser, classes);
+  }
+  return <GoogleLogin clientId="425993808005-tq26jpnsupbrupqvtcjn8es2nl3qkc95.apps.googleusercontent.com" onSuccess={onSuccess} onFailure={err => console.log('fail', err)} isSignedIn={true} />;
 };
 
 const styles = {
