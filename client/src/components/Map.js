3e9599b3fb3a15cf73a2544dace87f861aa19ca6
Vitor Boccio <vitorboccio@gmail.com>
2019-11-17 20:46:23 +1100

    adding pin to real location

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
+import PinIcon from './PinIcon';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -12,6 +13,20 @@ const INITIAL_VIEWPORT = {
 
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const {latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  }
 
   return (
     <div className={classes.root}>
@@ -26,6 +41,11 @@ const Map = ({ classes }) => {
         <div className={classes.navigationControl}>
           <NavigationControl onViewportChange={newViewport => setViewport(newViewport)} />
         </div>
+        {userPosition && (
+          <Marker latitude={userPosition.latitude} longitude={userPosition.longitude} offsetLeft={-19} offsetTop={-37}>
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
       </ReactMapGL>
     </div>
   );
