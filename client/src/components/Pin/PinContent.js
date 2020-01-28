562c2028379e5e69d635ec40b85c3ebfaa16b2af
Vitor Boccio <vitorboccio@gmail.com>
2020-01-28 15:45:02 +1100

    adding pin content

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
-// import AccessTime from "@material-ui/icons/AccessTime";
-// import Face from "@material-ui/icons/Face";
+import Typography from "@material-ui/core/Typography";
+import AccessTime from "@material-ui/icons/AccessTime";
+import Face from "@material-ui/icons/Face";
+import Context from '../../context';
+import format from 'date-fns/format';
 
 const PinContent = ({ classes }) => {
-  return <div>PinContent</div>;
+  const { state } = useContext(Context);
+  const { currentPin: { title, content, author, createdAt, comments } } = state;
+  return (
+    <div className={classes.root}>
+      <Typography component="h2" variant="h4" color="primary" gutterBottom>
+        { title }
+      </Typography>
+      <Typography className={classes.text} component="h3" variant="h5" color="inherit" gutterBottom >
+        <Face className={classes.icon}  /> {author.name}
+      </Typography>
+      <Typography className={classes.text} component="h3" variant="subtitle2" gutterBottom color="inherit">
+        <AccessTime className={classes.icon} />
+        {format(Number(createdAt), "Do MMM, YYYY")}
+      </Typography>
+      <Typography className={classes.text} variant="subtitle1" gutterBottom color="inherit">
+        {content}
+      </Typography>
+    </div>
+  )
+
 };
 
 const styles = theme => ({
